e4df30406dd3874dc96b1dad121c7cbb34b88605
吳 蕥綺 <n0975116268@gmail.com>
2023-12-29 13:39:46 +0800

    add other type http request example

--- src/main/java/com/example/jsonpractice/resttemplate/RestTemplateController.java
+++ src/main/java/com/example/jsonpractice/resttemplate/RestTemplateController.java
@@ -1,12 +1,13 @@
 package com.example.jsonpractice.resttemplate;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -29,6 +30,22 @@ public class RestTemplateController {
         System.out.println("student phone 值 " + student.getContactPhone());
         return "getForObject success";
 
+    }
+
+    @GetMapping("/getForObjectWithParam")
+    public String getForObjectWithParam() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> queryParamMap = new HashMap<>();
+        queryParamMap.put("graduate", true);
+
+        Student student = restTemplate.getForObject(
+                "https://mocki.io/v1/77f69435-224e-4c90-b7cd-53f07bbf5604",
+                Student.class,
+                queryParamMap
+        );
+
+        return "getForObject with param success";
     }
 
 /*
@@ -63,5 +80,82 @@ public class RestTemplateController {
             }
         });
         return "getForEntity success";
+    }
+
+    @GetMapping("/postForObject")
+    public String postForObject() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Student studentRequestBody = new Student();
+        studentRequestBody.setName("John");
+
+        Student result = restTemplate.postForObject(
+                "https://mocki.io/v1/b7a68eb2-f77c-43cf-bffe-8c647e9a2514",
+                studentRequestBody,
+                Student.class
+        );
+
+        return "postForObject success";
+    }
+
+    @GetMapping("/postForEntity")
+    public String postForEntity() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Student studentRequestBody = new Student();
+        studentRequestBody.setName("John");
+
+        ResponseEntity<Student> responseEntity = restTemplate.postForEntity(
+                "https://mocki.io/v1/b7a68eb2-f77c-43cf-bffe-8c647e9a2514",
+                studentRequestBody,
+                Student.class
+        );
+
+        return "postForEntity success";
+    }
+
+
+    @GetMapping("/exchange")
+    public String exchange() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 使用 exchange 發起 GET 請求
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("header1", "123");
+
+        HttpEntity requestEntity = new HttpEntity(requestHeaders);
+
+        Map<String, Object> queryParamMap = new HashMap<>();
+        queryParamMap.put("graduate", true);
+
+        ResponseEntity<Student> getStudentEntity = restTemplate.exchange(
+                "https://mocki.io/v1/b7a68eb2-f77c-43cf-bffe-8c647e9a2514",
+                HttpMethod.GET,
+                requestEntity,
+                Student.class,
+                queryParamMap
+        );
+
+
+
+        // 使用 exchange 發起 POST 請求
+        HttpHeaders requestHeaders2 = new HttpHeaders();
+        requestHeaders2.set("header2", "456");
+        requestHeaders2.setContentType(MediaType.APPLICATION_JSON);
+
+        Student studentRequestBody = new Student();
+        studentRequestBody.setName("John");
+
+        HttpEntity requestEntity2 = new HttpEntity(studentRequestBody, requestHeaders2);
+
+        ResponseEntity<Student> postStudentEntity = restTemplate.exchange(
+                "https://mocki.io/v1/b7a68eb2-f77c-43cf-bffe-8c647e9a2514",
+                HttpMethod.POST,
+                requestEntity2,
+                Student.class
+        );
+
+        return "exchange success";
     }
 }
